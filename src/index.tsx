8219f271bbbe12d031392611c3f3d257a8cd1f57
ShlemenKirill <kirill.shlemen@gmail.com>
2022-08-27 20:39:05 +0400

    feat: add local fonts

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import 'core/assets/fonts/Montserrat.ttf';
+import 'core/assets/fonts/STIXTwoText.ttf';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
