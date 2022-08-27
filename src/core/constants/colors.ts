c472fb130670c74ae684e7c8a9695f0a558e843e
ShlemenKirill <kirill.shlemen@gmail.com>
2022-08-27 13:29:40 +0400

    feat: add constants

---
+++ src/core/constants/colors.ts
@@ -0,0 +1,7 @@
+export const colors = {
+  violet: '#662C91',
+  violetDark: '#4A2169',
+  grey: '#787878',
+  matteBlack: '#1F1F1F',
+  white: '#FFFFFF',
+};
---
+++ src/core/constants/fonts.ts
@@ -0,0 +1,30 @@
+export const fonts = {
+  stixNormal24: {
+    fontFamily: 'STIX Two Text',
+    fontStyle: 'normal',
+    fontWeight: '600',
+    fontSize: '24px',
+    lineHeight: '17px',
+  },
+  montserratNormal14: {
+    fontFamily: 'Montserrat',
+    fontStyle: 'normal',
+    fontWeight: '400',
+    fontSize: '14px',
+    lineHeight: '17px',
+  },
+  montserratMedium16: {
+    fontFamily: 'Montserrat',
+    fontStyle: 'normal',
+    fontWeight: '500',
+    fontSize: '16px',
+    lineHeight: '25px',
+  },
+  montserratBold16: {
+    fontFamily: 'Montserrat',
+    fontStyle: 'normal',
+    fontWeight: '700',
+    fontSize: '16px',
+    lineHeight: '20px',
+  },
+};
