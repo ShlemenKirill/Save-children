4e83fda0361584a124dec5255c245c237bc1f269
ShlemenKirill <kirill.shlemen@gmail.com>
2022-08-27 20:43:23 +0400

    fix: correct routes

--- src/core/constants/navigation.ts
+++ src/core/constants/navigation.ts
@@ -1,5 +1,5 @@
-export const ABOUT_US = 'about';
-export const REPORTS = 'reports';
-export const PARTNERS = 'partners';
-export const VOLUNTEERS = 'volunteers';
-export const CONTACTS = 'contacts';
+export const ABOUT_US = '/about';
+export const REPORTS = '/reports';
+export const PARTNERS = '/partners';
+export const VOLUNTEERS = '/volunteers';
+export const CONTACTS = '/contacts';
