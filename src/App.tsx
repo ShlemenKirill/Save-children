0a394c641fa8f09987e3040500edfc098dc95566
ShlemenKirill <kirill.shlemen@gmail.com>
2022-08-27 20:41:02 +0400

    feat: add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,33 @@
-import React from 'react';
-import Header from 'components/Header';
+import React, { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { NavigationMenuItems } from 'core/constants/common';
+import { ABOUT_US, CONTACTS, PARTNERS, REPORTS, VOLUNTEERS } from 'core/constants/navigation';
+import Main from 'pages/Main';
+import Reports from 'pages/Reports';
+import Partners from 'pages/Partners';
+import Volunteers from 'pages/Volunteers';
+import Contacts from 'pages/Contacts';
+import About from 'pages/About';
+import MenuContext from 'context/MenuContext';
 
-function App() {
-  return <Header />;
-}
+const App = () => {
+  const [currentMenuItem, setCurrentMenuItem] = useState<NavigationMenuItems>(
+    NavigationMenuItems.main,
+  );
+  return (
+    <MenuContext.Provider value={{ currentMenuItem, setCurrentMenuItem }}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Main />} />
+          <Route path={ABOUT_US} element={<About />} />
+          <Route path={REPORTS} element={<Reports />} />
+          <Route path={PARTNERS} element={<Partners />} />
+          <Route path={VOLUNTEERS} element={<Volunteers />} />
+          <Route path={CONTACTS} element={<Contacts />} />
+        </Routes>
+      </BrowserRouter>
+    </MenuContext.Provider>
+  );
+};
 
 export default App;
