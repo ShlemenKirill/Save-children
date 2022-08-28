4f35e7a733584cdaff7f22bd926b88ef88ed3226
ShlemenKirill <kirill.shlemen@gmail.com>
2022-08-28 14:14:04 +0400

    Update StyledButton.tsx

--- src/components/StyledButton/StyledButton.tsx
+++ src/components/StyledButton/StyledButton.tsx
@@ -20,7 +20,27 @@ const StyledButton = ({ buttonVariant = ButtonVariants.primary, children }: ISty
             height: '45px',
             '&:hover': {
               background: colors.violet,
-              filter: 'drop-shadow(0px 6px 21px rgba(102, 44, 145, 0.5))',
+              filter: 'drop-shadow(0px 6px 16px rgba(102, 44, 145, 0.4))',
+            },
+          }}
+        >
+          {children}
+        </Button>
+      );
+    case ButtonVariants.primaryGreen:
+      return (
+        <Button
+          variant='contained'
+          sx={{
+            textTransform: 'none',
+            background: colors.green,
+            ...fonts.montserratBold16,
+            minWidth: '200px',
+            width: 'max-content',
+            height: '45px',
+            '&:hover': {
+              background: colors.green,
+              filter: 'drop-shadow(0px 6px 16px rgba(117, 152, 61, 0.4))',
             },
           }}
         >
@@ -41,7 +61,7 @@ const StyledButton = ({ buttonVariant = ButtonVariants.primary, children }: ISty
             height: '45px',
             '&:hover': {
               border: `1px solid ${colors.violetDark}`,
-              filter: 'drop-shadow(0px 6px 21px rgba(102, 44, 145, 0.5))',
+              filter: 'drop-shadow(0px 6px 16px rgba(102, 44, 145, 0.4))',
             },
           }}
         >
