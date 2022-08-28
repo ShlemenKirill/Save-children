e352edb9a4e4cedcb93173d2300d2b187e7d1e48
ShlemenKirill <kirill.shlemen@gmail.com>
2022-08-28 14:13:45 +0400

    feat: add fonts, colors, types

--- src/core/constants/fonts.ts
+++ src/core/constants/fonts.ts
@@ -2,29 +2,44 @@ export const fonts = {
   stixNormal24: {
     fontFamily: 'STIXTwoText',
     fontStyle: 'normal',
-    fontWeight: '600',
+    fontWeight: 600,
     fontSize: '24px',
     lineHeight: '17px',
+  },
+  stixSemibold90: {
+    fontFamily: 'STIXTwoText',
+    fontStyle: 'normal',
+    fontWeight: 600,
+    fontSize: '90px',
+    lineHeight: '85px',
   },
   montserratNormal14: {
     fontFamily: 'Montserrat',
     fontStyle: 'normal',
-    fontWeight: '400',
+    fontWeight: 400,
     fontSize: '14px',
     lineHeight: '17px',
+  },
+  montserratNormal24: {
+    fontFamily: 'Montserrat',
+    fontStyle: 'normal',
+    fontWeight: 400,
+    fontSize: '24px',
+    lineHeight: '36px',
   },
   montserratMedium16: {
     fontFamily: 'Montserrat',
     fontStyle: 'normal',
-    fontWeight: '500',
+    fontWeight: 500,
     fontSize: '16px',
     lineHeight: '25px',
   },
   montserratBold16: {
     fontFamily: 'Montserrat',
     fontStyle: 'normal',
-    fontWeight: '700',
+    fontWeight: 700,
     fontSize: '16px',
     lineHeight: '20px',
   },
+
 };
