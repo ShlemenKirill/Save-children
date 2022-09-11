6133fc2dded0ecaa4b40dccae9ae687878b2072d
ShlemenKirill <kirill.shlemen@gmail.com>
2022-09-11 22:02:40 +0400

    feat: add lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { lazy, useState, Suspense } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { CircularProgress } from '@mui/material';
 import { NavigationMenuItems } from 'core/constants/common';
 import {
   ABOUT_US,
@@ -9,18 +10,19 @@ import {
   REPORTS,
   VOLUNTEERS,
 } from 'core/constants/navigation';
-import Main from 'pages/Main';
-import Reports from 'pages/Reports';
-import Partners from 'pages/Partners';
-import Volunteers from 'pages/Volunteers';
-import Contacts from 'pages/Contacts';
-import About from 'pages/About';
-import Projects from 'pages/Projects';
-import PregnantHelp from 'pages/Projects/PregnantHelp';
-import LargeFamilyHelp from 'pages/Projects/LargeFamilyHelp';
-import CareHome from 'pages/Projects/CareHome';
-import HelpInEvacuation from 'pages/Projects/HelpInEvacuation';
 import MenuContext from 'context/MenuContext';
+
+const Main = lazy(() => import('pages/Main'));
+const Reports = lazy(() => import('pages/Reports'));
+const Partners = lazy(() => import('pages/Partners'));
+const Volunteers = lazy(() => import('pages/Volunteers'));
+const Contacts = lazy(() => import('pages/Contacts'));
+const About = lazy(() => import('pages/About'));
+const Projects = lazy(() => import('pages/Projects'));
+const PregnantHelp = lazy(() => import('pages/Projects/PregnantHelp'));
+const LargeFamilyHelp = lazy(() => import('pages/Projects/LargeFamilyHelp'));
+const CareHome = lazy(() => import('pages/Projects/CareHome'));
+const HelpInEvacuation = lazy(() => import('pages/Projects/HelpInEvacuation'));
 
 const App = () => {
   const [currentMenuItem, setCurrentMenuItem] = useState<NavigationMenuItems>(
@@ -30,19 +32,96 @@ const App = () => {
     <MenuContext.Provider value={{ currentMenuItem, setCurrentMenuItem }}>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Main />} />
-          <Route path={ABOUT_US} element={<About />} />
+          <Route
+            path='/'
+            element={
+              <Suspense fallback={<CircularProgress color='secondary' />}>
+                <Main />
+              </Suspense>
+            }
+          />
+          <Route
+            path={ABOUT_US}
+            element={
+              <Suspense fallback={<CircularProgress color='secondary' />}>
+                <About />
+              </Suspense>
+            }
+          />
           <Route path={PROJECTS}>
-            <Route index element={<Projects />} />
-            <Route path={'pregnant-help'} element={<PregnantHelp />} />
-            <Route path={'large-family-help'} element={<LargeFamilyHelp />} />
-            <Route path={'care-home'} element={<CareHome />} />
-            <Route path={'help-in-evacuation'} element={<HelpInEvacuation />} />
+            <Route
+              index
+              element={
+                <Suspense fallback={<CircularProgress color='secondary' />}>
+                  <Projects />
+                </Suspense>
+              }
+            />
+            <Route
+              path={'pregnant-help'}
+              element={
+                <Suspense fallback={<CircularProgress color='secondary' />}>
+                  <PregnantHelp />
+                </Suspense>
+              }
+            />
+            <Route
+              path={'large-family-help'}
+              element={
+                <Suspense fallback={<CircularProgress color='secondary' />}>
+                  <LargeFamilyHelp />
+                </Suspense>
+              }
+            />
+            <Route
+              path={'care-home'}
+              element={
+                <Suspense fallback={<CircularProgress color='secondary' />}>
+                  <CareHome />
+                </Suspense>
+              }
+            />
+            <Route
+              path={'help-in-evacuation'}
+              element={
+                <Suspense fallback={<CircularProgress color='secondary' />}>
+                  <HelpInEvacuation />
+                </Suspense>
+              }
+            />
           </Route>
-          <Route path={REPORTS} element={<Reports />} />
-          <Route path={PARTNERS} element={<Partners />} />
-          <Route path={VOLUNTEERS} element={<Volunteers />} />
-          <Route path={CONTACTS} element={<Contacts />} />
+          <Route
+            path={REPORTS}
+            element={
+              <Suspense fallback={<CircularProgress color='secondary' />}>
+                <Reports />
+              </Suspense>
+            }
+          />
+          <Route
+            path={PARTNERS}
+            element={
+              <Suspense fallback={<CircularProgress color='secondary' />}>
+                <Partners />
+              </Suspense>
+            }
+          />
+          <Route
+            path={VOLUNTEERS}
+            element={
+              <Suspense fallback={<CircularProgress color='secondary' />}>
+                <Volunteers />
+              </Suspense>
+            }
+          />
+          <Route
+            path={CONTACTS}
+            element={
+              <Suspense fallback={<CircularProgress color='secondary' />}>
+                <Contacts />
+              </Suspense>
+            }
+          />
         </Routes>
       </BrowserRouter>
     </MenuContext.Provider>
