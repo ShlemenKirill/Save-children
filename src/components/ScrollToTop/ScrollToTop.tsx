965eb0f35b1cb38a199932b0bd07838679f88808
ShlemenKirill <kirill.shlemen@gmail.com>
2022-09-11 21:35:29 +0400

    feat: update ScrollToTop component

--- src/components/ScrollToTop/ScrollToTop.tsx
+++ src/components/ScrollToTop/ScrollToTop.tsx
@@ -4,7 +4,7 @@ import { useEffect } from 'react';
 const ScrollToTop = () => {
   const { pathname } = useLocation();
   useEffect(() => {
-    window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+    window.scrollTo({ top: 0, left: 0 });
   }, [pathname]);
   return null;
 };
