66b5c30cfabf552a6a9dab0d30b46d9408b80be5
ShlemenKirill <kirill.shlemen@gmail.com>
2023-06-01 15:19:43 +0200

    fix: eslint errors

--- src/pages/Main/Components/HelpAtWar/HelpAtWar.tsx
+++ src/pages/Main/Components/HelpAtWar/HelpAtWar.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React from 'react';
 import { Box, Grid, List, ListItem, Typography } from '@mui/material';
 import { colors } from 'core/constants/colors';
 import { fonts } from 'core/constants/fonts';
